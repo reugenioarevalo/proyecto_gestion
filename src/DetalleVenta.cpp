4ef5d7cba7ef949b69c0b0b3ec47513475c604f1
Eugenio <reugenioarevalo@gmail.com>
2020-11-03 11:12:52 -0300

    ajustes en decimales

--- src/DetalleVenta.cpp
+++ src/DetalleVenta.cpp
@@ -286,9 +286,9 @@ void DetalleVenta::imprimirFactura(){
                 cout<<"|"<< setw(28)<<aux.prod.getDescripcion();
                 cout<<right;
                 cout<<"|"<< setw(10)<<centrarInt(aux.getCantProducto(), 10);
-                cout<<"|"<< setw(10)<<aux.getPrecio();
-                 cout<<"|"<< setw(10)<<(aux.getPrecio()*prod1->getIva()/100);
-                 cout<<"|"<<setw(16)<<((aux.getPrecio()+(aux.getPrecio()*prod1->getIva()/100))*aux.getCantProducto())<<"|"<<endl;
+                cout<<"|"<< setw(10)<<fixed<<setprecision(2)<<aux.getPrecio();
+                 cout<<"|"<< setw(10)<<fixed<<setprecision(2)<<(aux.getPrecio()*prod1->getIva()/100);
+                 cout<<"|"<<setw(16)<<fixed<<setprecision(2)<<((aux.getPrecio()+(aux.getPrecio()*prod1->getIva()/100))*aux.getCantProducto())<<"|"<<endl;
 
                 tPrUn+=aux.getPrecio();
                 sTot+=(aux.getPrecio()*aux.getCantProducto());
@@ -301,9 +301,9 @@ void DetalleVenta::imprimirFactura(){
 //            cout<<"|"<<setw(89)<<setfill('+')<<"|"<<endl;
             cout<<"|"<<setw(89)<<setfill(' ')<<"|"<<endl;
             cout<< right;
-            cout<<"|"<<setw(80)<<"SubTotal:$"<<setw(8)<<derechafloat(sTot,8)<<"|"<<endl;
-            cout<<"|"<<setw(80)<<"Total Iva:$"<<setw(8)<<derechafloat(sIva,8)<<"|"<<endl;
-            cout<<"|"<<setw(80)<<"Total Final:$"<<setw(8)<<derechafloat(tTot,8)<<"|"<<endl;
+            cout<<"|"<<setw(80)<<"SubTotal:$"<<setw(8)<<fixed<<setprecision(2)<<sTot<<"|"<<endl;
+            cout<<"|"<<setw(80)<<"Total Iva:$"<<setw(8)<<fixed<<setprecision(2)<<sIva<<"|"<<endl;
+            cout<<"|"<<setw(80)<<"Total Final:$"<<setw(8)<<fixed<<setprecision(2)<<tTot<<"|"<<endl;
             cout<<"|"<<setw(89)<<setfill(' ')<<"|"<<endl;
 //            cout<<"|"<<setw(89)<<setfill('+')<<"|"<<endl;
             cout<<"|"<<setw(89)<<setfill('_')<<"|"<<endl;
